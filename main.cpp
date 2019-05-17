27c99c8d330475f8b07e83bbc392541615f12a86
Marcin Rał <marcin@ral.info.pl>
2019-05-17 17:24:34 +0200

    added comments

--- main.cpp
+++ main.cpp
@@ -4,16 +4,20 @@ Wykona³:
 */
 #include <iostream> // do³¹czanie zewnêtrznej biblioteki - iostream 
 using namespace std; // u¿ywamy standardowej przestrzeni nazw
-int main(int argc, char** argv) {
-	cout << "Hello world" << endl;
-	cout <<     "Jestem twoim komputerem :)" << endl;
-    for (int i=0;i<10;i++)	{
-    	for (int j=0;j<i;j++) {
-    		cout << "*"  ;
+int main(int argc, char** argv) { // g³ówna funkcja w c++
+	cout << "Hello world" << endl; // wyœwietlanie napisu
+	cout <<     "Jestem twoim komputerem :)" << endl; // wyœwietlanie napisu
+    
+	for (int i=0;i<10;i++)	{ // pêtla zewnêtrzna
+    
+    	for (int j=0;j<i;j++) { // pêtla wewnêtrzna
+    		cout << "*"  ;   // wyœwietlanie napisu
 		}
-        cout <<	endl ;
+		
+//		cout << "Enter->";
+        cout <<	endl ; // przejœcie do nowej lini 
 	}
+	
 	return 0;
 }
-
 
