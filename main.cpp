39c1eb36a23cbe1c2b6586864eeb5eb09d749fe1
Marcin Rał <marcin@ral.info.pl>
2019-05-17 18:08:28 +0200

    christmass tree

--- main.cpp
+++ main.cpp
@@ -7,17 +7,19 @@ using namespace std; // u¿ywamy standardowej przestrzeni nazw
 int main(int argc, char** argv) { // g³ówna funkcja w c++
 	cout << "Hello world" << endl; // wyœwietlanie napisu
 	cout <<  "Jestem twoim komputerem :)" << endl; // wyœwietlanie napisu
-    int i=10;
-//	for (;i<10;i++)	{ // pêtla zewnêtrzna
-    
-    	for (int j=0;j<i;j++) { // pêtla wewnêtrzna
-    		cout << j << " ) *" <<  endl   ;   // wyœwietlanie napisu 
+    int i=0;
+	for (;i<10;i++)	{ // pêtla zewnêtrzna
+    	for (int s=(10-i);s>0;s--) { // pêtla wewnêtrzna
+    		cout << " " ;   // wyœwietlanie spacji przed gwiazdkami 
 		}
-	//	cout << "+" ;
-//		cout << "Enter->";
-//        cout <<	endl ; // przejœcie do nowej lini 
-//	}
-	
+		for (int j=0;j<i;j = j + 1) { // pêtla wewnêtrzna
+    		cout << "*" ;   // wyœwietlanie gwiazdek z lewej 
+		}
+		for (int j=0;j<i;j = j + 1) { // pêtla wewnêtrzna
+    		cout << "*" ;   // wyœwietlanie gwiazdek z prawej 
+		}
+	    cout <<	endl ; // przejœcie do nowej lini 
+	}
 	return 0;
 }
 
