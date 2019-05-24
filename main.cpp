e640b9a2c36829eed04021f6252f352cd5b44822
Marcin Rał <marcin@ral.info.pl>
2019-05-24 17:07:41 +0200

    lejek

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ int main(int argc, char** argv) { // g³ówna funkcja w c++
 	cout <<  "Jestem twoim komputerem :)" << endl; // wyœwietlanie napisu
     int i=0;
 	for (;i<10;i++)	{ // pêtla zewnêtrzna
-    	for (int s=(10-i);s>0;s--) { // pêtla wewnêtrzna
+    	for (int s=(0);s<i;s++) { // pêtla wewnêtrzna
     		cout << " " ;   // wyœwietlanie spacji przed gwiazdkami 
 		}
-		for (int j=0;j<i;j = j + 1) { // pêtla wewnêtrzna
+		for (int j=10;j>i;j = j - 1) { // pêtla wewnêtrzna
     		cout << "*" ;   // wyœwietlanie gwiazdek z lewej 
 		}
-		for (int j=0;j<i;j = j + 1) { // pêtla wewnêtrzna
+		for (int j=10;j>i;j = j - 1) { // pêtla wewnêtrzna
     		cout << "*" ;   // wyœwietlanie gwiazdek z prawej 
 		}
 	    cout <<	endl ; // przejœcie do nowej lini 
