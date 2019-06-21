f0a09957297db3109015c45b681b741b742619de
Marcin Rał <marcin@ral.info.pl>
2019-06-21 16:12:12 +0200

    dodane funkcje

--- tree/main.cpp
+++ tree/main.cpp
@@ -1,20 +1,8 @@
 #include <iostream> // do³¹czanie zewnêtrznej biblioteki - iostream 
 using namespace std; // u¿ywamy standardowej przestrzeni nazw
-int main(int argc, char** argv) { // g³ówna funkcja w c++
-	char znak;
-	while(znak != 'q'){ // dopóki  znak nie jest równy 'q' 
 
-		cout << "Wybierz pozycje z menu:" << endl;
-		cout << "q) aby wyjsc" << endl;
-		cout << "1) rysuj choinke" << endl;
-		cout << "2) rysuj lejek" << endl;		
-		cout << "3) rysuj krysztal" << endl;		
-		cout << "4) rysuj ..." << endl;				
-		cin >> znak;    // pobierz znak z klawiatury i zapisz do zmiennej znak
-		cout << "wpisales " << znak << endl;
-		
-		if(znak == '2'){
-			int i = 0;
+void lejek(){
+	int i = 0;
 			for (;i<10;i++)	{ // pêtla zewnêtrzna
 		    	for (int s=(0);s<i;s++) { // pêtla wewnêtrzna
 		    		cout << " " ;   // wyœwietlanie spacji przed gwiazdkami 
@@ -27,10 +15,10 @@ int main(int argc, char** argv) { // g³ówna funkcja w c++
 				}
 	    		cout <<	endl ; // przejœcie do nowej lini 
 			}
-		}
-		
-		if(znak == '1'){
-			 int i=0;
+}
+void choinka(){
+	 int i=0;
+			 
 			for (;i<10;i++)	{ // pêtla zewnêtrzna
 		    	for (int s=(10-i);s>0;s--) { // pêtla wewnêtrzna
 		    		cout << " " ;   // wyœwietlanie spacji przed gwiazdkami 
@@ -44,74 +32,37 @@ int main(int argc, char** argv) { // g³ówna funkcja w c++
 			    cout <<	endl ; // przejœcie do nowej lini 
 					
 				}
+}
+int main(int argc, char** argv) { // g³ówna funkcja w c++
+	char znak;
+	while(znak != 'q'){ // dopóki  znak nie jest równy 'q' 
+
+		cout << "Wybierz pozycje z menu:" << endl;
+		cout << "q) aby wyjsc" << endl;
+		cout << "1) rysuj choinke" << endl;
+		cout << "2) rysuj lejek" << endl;		
+		cout << "3) rysuj kolce" << endl;		
+		cout << "4) rysuj krysztal" << endl;				
+		cin >> znak;    // pobierz znak z klawiatury i zapisz do zmiennej znak
+		cout << "wpisales " << znak << endl;
+		
+		
+		if(znak == '1'){
+			choinka();
 		}
+		
+		if(znak == '2'){
+			lejek();
+		}
+		
 		if(znak == '3'){
-			int i=0;
-			for (;i<10;i++)	{ // pêtla zewnêtrzna
-		    	for (int s=(10-i);s>0;s--) { // pêtla wewnêtrzna
-		    		cout << " " ;   // wyœwietlanie spacji przed gwiazdkami 
-				}
-				for (int j=0;j<i;j = j + 1) { // pêtla wewnêtrzna
-		    		cout << "*" ;   // wyœwietlanie gwiazdek z lewej 
-				}
-				for (int j=0;j<i;j = j + 1) { // pêtla wewnêtrzna
-		    		cout << "*" ;   // wyœwietlanie gwiazdek z prawej 
-				}
-			    cout <<	endl ; // przejœcie do nowej lini}
-//	             {
-//				}
-	    		cout <<	endl ; // przejœcie do nowej lini
-//	    		{
-				int i=0;
-				for (;i<10;i++)	{ // pêtla zewnêtrzna
-			    	for (int s=(0);s<i;s++) { // pêtla wewnêtrzna
-			    		cout << " " ;   // wyœwietlanie spacji przed gwiazdkami 
-					}
-					for (int j=10;j>i;j = j - 1) { // pêtla wewnêtrzna
-			    		cout << "*" ;   // wyœwietlanie gwiazdek z lewej 
-					}
-					for (int j=10;j>i;j = j - 1) { // pêtla wewnêtrzna
-			    		cout << "*" ;   // wyœwietlanie gwiazdek z prawej 
-					}
-		    	cout <<	endl ; // przejœcie do nowej lini
-		    	
-			}
-//				 }
-			}
+
 		}
 		if(znak == '4'){
-			// narysowac krysztal
-			
-			for (int i=0;i<10;i++)	{ // pêtla zewnêtrzna
-		    	for (int s=(10-i);s>0;s--) { // pêtla wewnêtrzna
-		    		cout << " " ;   // wyœwietlanie spacji przed gwiazdkami 
-				}
-				for (int j=0;j<i;j = j + 1) { // pêtla wewnêtrzna
-		    		cout << "*" ;   // wyœwietlanie gwiazdek z lewej 
-				}
-				for (int j=0;j<i;j = j + 1) { // pêtla wewnêtrzna
-		    		cout << "*" ;   // wyœwietlanie gwiazdek z prawej 
-				}
-			    cout <<	endl ; // przejœcie do nowej lini 
-				}
-			
-			for (int i=0;i<10;i++)	{ // pêtla zewnêtrzna
-		    	for (int s=(0);s<i;s++) { // pêtla wewnêtrzna
-		    		cout << " " ;   // wyœwietlanie spacji przed gwiazdkami 
-				}
-				for (int j=10;j>i;j = j - 1) { // pêtla wewnêtrzna
-		    		cout << "*" ;   // wyœwietlanie gwiazdek z lewej 
-				}
-				for (int j=10;j>i;j = j - 1) { // pêtla wewnêtrzna
-		    		cout << "*" ;   // wyœwietlanie gwiazdek z prawej 
-				}
-	    		cout <<	endl ; // przejœcie do nowej lini 		
-				}	
+			choinka();
+			lejek();
 		}
 
 }
 return 0;
 }
-
-
-
